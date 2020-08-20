a53197dc5f614bcba83d85503e4db70af358ec7c
Rasmus <rasmus@nolimitation.dk>
2020-08-20 17:29:33 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Dashboard from './components/dashboard';
+import Dashboard from './components/dashboard/dashboard';
+import Navbar from './components/layouts/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/'component={Dashboard} />
+        </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
